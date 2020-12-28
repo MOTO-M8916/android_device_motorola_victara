d653e7f5e7f0205d7d82c10fac53d4c05a22b2d4
althafvly <althafvly@gmail.com>
2020-12-28 11:26:55 +0530

    Update stuff

--- omni_victara.mk
+++ omni_victara.mk
@@ -15,20 +15,14 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Release name
+PRODUCT_RELEASE_NAME := victara
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-PRODUCT_PACKAGES += \
-    qcom_decrypt
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := victara
